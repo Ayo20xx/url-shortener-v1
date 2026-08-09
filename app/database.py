030d3b385ff6193a540426fd13db0d0f6141d85d
Ayo20xx <ayomidevictoraaaa@gmail.com>
2026-08-09 20:36:59 +0100

    changes: add dotenv to load credentials

---
+++ app/database.py
@@ -0,0 +1,11 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+load_dotenv()
+
+
+db_connect = os.environ.data
+
+
--- app/model.py
+++ app/model.py
@@ -1,7 +1,1 @@
-from pydantic import AnyHttpUrl, BaseModel
-
-
-class url (BaseModel):
-    id : int 
-    short_code : str
-    url : AnyHttpUrl
+import fastapi
