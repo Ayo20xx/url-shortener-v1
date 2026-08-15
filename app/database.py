aae18288a8d820a247a275143522f59e8c4b3a98
Ayo20xx <ayomidevictoraaaa@gmail.com>
2026-08-15 12:51:43 +0100

    database model

--- app/database.py
+++ app/database.py
@@ -11,18 +11,18 @@ engine= create_async_engine(
 
 async def create_db_tables():
     async with engine.begin() as connection:
-        from app.model  # noqa: F401
+        from app.model import Url # noqa: F401
         await connection.run_sync(SQLModel.metadata.create_all)
 
 
 
-async def get_session ():
-    asyncsession= async_sessionmaker(
+asyncsession= async_sessionmaker(
         bind= engine,
         class_= AsyncSession,
         expire_on_commit= False
     )
 
 
-
-
+async def get_session ():
+    async with asyncsession as session:
+        yield session
--- app/model.py
+++ app/model.py
@@ -1,1 +1,11 @@
-import fastapi
+from datetime import datetime, timedelta, timezone
+
+from sqlmodel import Field, SQLModel
+
+
+class Url (SQLModel,table=True):
+    id : int | None = Field(default=None,primary_key=True)
+    url : str
+    shortcode : str = Field( unique=True, index= True)
+    created_at : datetime = Field(default_factory=lambda:datetime.now(timezone.utc))
+    expires_at : datetime |None  = Field (default_factory= lambda: datetime.now(timezone.utc) +timedelta(days=30) )
